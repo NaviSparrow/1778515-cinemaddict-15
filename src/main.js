07d611b6a7b474ad4f2ab673129e2009c6eaefa7
Ivan <work.gorobec@gmail.com>
2021-08-02 13:47:04 +0500

    исправляет замечания

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createFilmsBlockTemplate } from './view/films-block-markup.js';
+import { createFilmsListTemplate } from './view/films-list.js';
 import { createFilmCardTemplate } from './view/film-card.js';
 import { createMainMenuTemplate } from './view/main-menu.js';
 import { createPopupTemplate } from './view/popup.js';
@@ -6,46 +6,56 @@ import { createShowMoreButtonTemplate } from './view/show-more-button.js';
 import { createSortTemplate } from './view/sort.js';
 import { createStatisticsTemplate } from './view/statistics.js';
 import { createUserProfileTemplate } from './view/user.js';
+import { createFilmsSectionTemplate } from './view/films.js';
+import { createFilmsListExtraTemplate } from './view/films-list-extra.js';
 
-const TASK_COUNT = 5;
-const EXTRA_TASK_COUNT = 2;
-const FILMS_SECTION = 0;
-const TOP_RATED_SECTION = 1;
-const MOST_COMMENTED_SECTION = 2;
+const CARDS_COUNT = 5;
+const EXTRA_CARDS_COUNT = 2;
+const TOP_RATED_FILMS = 0;
+const MOST_COMMENTED_FILMS = 1;
+const RenderPlace = {
+  beforebegin: 'beforebegin',
+  afterbegin: 'afterbegin',
+  afterend: 'afterend',
+};
 
-const mainMenu = document.querySelector('.main');
+const mainSection = document.querySelector('.main');
 const userProfile = document.querySelector('.header');
 const footerStatistics = document.querySelector('.footer__statistics');
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
+const render = (container, template, place = 'beforeend') => container.insertAdjacentHTML(place, template);
+
+const renderExtraSection = (container) => {
+  for (let i = 0; i < EXTRA_CARDS_COUNT; i++) {
+    render(container, createFilmCardTemplate());
+  }
 };
 
-render(userProfile, createUserProfileTemplate(), 'beforeend');
-render(mainMenu, createMainMenuTemplate(), 'beforeend');
-render(mainMenu, createSortTemplate(), 'beforeend');
-render(mainMenu, createFilmsBlockTemplate(), 'beforeend');
+render(userProfile, createUserProfileTemplate());
+render(mainSection, createMainMenuTemplate());
+render(mainSection, createSortTemplate());
 
-const filmsBlock = document.querySelector('.films').children;
-const filmsList = filmsBlock[FILMS_SECTION];
-const filmsListContainer = filmsList.querySelector('.films-list__container');
-for(let i = 0 ; i < TASK_COUNT; i++) {
-  render(filmsListContainer, createFilmCardTemplate(), 'beforeend');
+render(mainSection, createFilmsSectionTemplate());
+const filmsSection = document.querySelector('.films');
+
+render(filmsSection, createFilmsListTemplate());
+
+const filmsListSection = filmsSection.querySelector('.films-list');
+const filmsListContainer = filmsListSection.querySelector('.films-list__container');
+for (let i = 0; i < CARDS_COUNT; i++) {
+  render(filmsListContainer, createFilmCardTemplate());
 }
 
-render(filmsList, createShowMoreButtonTemplate(), 'beforeend');
+render(filmsListSection, createShowMoreButtonTemplate());
 
-const topRatedFilms = filmsBlock[TOP_RATED_SECTION];
-const topRatedListContainer = topRatedFilms.querySelector('.films-list__container');
-for(let i = 0 ; i < EXTRA_TASK_COUNT; i++) {
-  render(topRatedListContainer, createFilmCardTemplate(), 'beforeend');
-}
+render(filmsSection, createFilmsListExtraTemplate());
+const extraSections = filmsSection.querySelectorAll('.films-list--extra');
 
-const mostCommentedFilms = filmsBlock[MOST_COMMENTED_SECTION];
-const mostCommentedListContainer = mostCommentedFilms.querySelector('.films-list__container');
-for(let i = 0 ; i < EXTRA_TASK_COUNT; i++) {
-  render(mostCommentedListContainer, createFilmCardTemplate(), 'beforeend');
-}
+const topRatedfilms = extraSections[TOP_RATED_FILMS].querySelector('.films-list__container');
+const mostCommentedFilms = extraSections[MOST_COMMENTED_FILMS].querySelector('.films-list__container');
+renderExtraSection(topRatedfilms);
+renderExtraSection(mostCommentedFilms);
 
-render(footerStatistics, createStatisticsTemplate(), 'beforeend');
-render(document.body, createPopupTemplate(), 'beforeend');
+
+render(footerStatistics, createStatisticsTemplate());
+// render(document.body, createPopupTemplate());
