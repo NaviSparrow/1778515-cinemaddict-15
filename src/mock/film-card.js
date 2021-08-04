65f343eb8b94c4b0ff9265b2531db98f8cead339
Ivan <work.gorobec@gmail.com>
2021-08-04 16:50:12 +0500

    добавляет поля для расширенной версии карточки в попапе

--- src/mock/film-card.js
+++ src/mock/film-card.js
@@ -1,3 +1,6 @@
+import dayjs from 'dayjs';
+import {getRandomFloat, getRandomArray, getRandomInteger, getRandomArrayElement } from '../utils/utils.js';
+
 const TITLES = [
   'Made for Each Other',
   'Popeye the Sailor Meets Sinbad the Sailor',
@@ -40,24 +43,65 @@ const DESRIPTIONS = [
   'In rutrum ac purus sit amet tempus.',
 ];
 
-const getRandomFloat = (min, max, precision = 0) => {
-  const result = (Math.random() * (max - min + 0.1)) + min;
-  return Number(result.toFixed(precision));
-};
-const getRandomInteger = (min, max) => getRandomFloat(min, max);
+const DIRECTORS = [
+  'Steven Spielberg',
+  'Martin Scorsese',
+  'Alfred Hitchcock',
+  'Stanley Kubrick',
+  'Quentin Tarantino',
+  'Francis Ford Coppola',
+  'Sergio Leone',
+];
 
-const getRandomArrayElement = (array) => array[getRandomInteger(0, array.length - 1)];
+const WRITERS = [
+  'Billy Wilder',
+  'Ethan Coen',
+  'Robert Towne',
+  'Quentin Tarantino',
+  'Francis Ford Coppola',
+  'William Goldman',
+  'Charlie Kaufman',
+  'Woody Allen',
+  'Nora Ephron',
+];
 
-const getRandomArray = (array, length) => {
-  const newArray = array.slice().sort(() => Math.random() - 0.5);
-  return newArray.slice(0, getRandomInteger(0, length));
-};
+const ACTORS = [
+  'Erich von Stroheim',
+  'Heinz Herald',
+  'Richard Weil',
+  'Jack Nicholson',
+  'Marlon Brando',
+  'Robert De Niro',
+  'Al Pacino',
+  'Daniel Day-Lewis',
+];
+
+const COUNTRES = [
+  'USA',
+  'France',
+  'Italy',
+  'Spain',
+  'Germany',
+];
+
+const AGE_RATINGS = [
+  '0+',
+  '12+',
+  '16+',
+  '18+',
+];
 
 const generateTitle = () => getRandomArrayElement(TITLES);
 
 const generateRating = () => getRandomFloat(1, 10, 1);
 
-const generateYear = () => getRandomInteger(1930, 1970);
+const generateYear = () => {
+  const year = '1950';
+  dayjs(year);
+  const maxYearGap = 20;
+  const yearGap = getRandomInteger(-maxYearGap, maxYearGap);
+  return dayjs(year).add(yearGap, 'year').toDate();
+};
 
 const generateDuration = () => {
   const hours = getRandomInteger(0, 2);
@@ -66,12 +110,11 @@ const generateDuration = () => {
     return `${minutes}m`;
   } else if (minutes === 0) {
     return `${hours}h`;
-  } else {
-    return `${hours}h ${minutes}m`;
   }
+  return `${hours}h ${minutes}m`;
 };
 
-const generateGanre = () => getRandomArrayElement(GANRES);
+const generateGanre = () => getRandomArray(GANRES, 4);
 
 const generatePoster = () => `./images/posters/${getRandomArrayElement(POSTERS)}`;
 
@@ -79,6 +122,29 @@ const generateDesription = () => getRandomArray(DESRIPTIONS, 5);
 
 const generateComments = () => getRandomInteger(0, 5);
 
+const generateOriginalTitle = () => generateTitle();
+
+const generateDirector = () =>  getRandomArrayElement(DIRECTORS);
+
+const generateWriters = () => getRandomArray(WRITERS, 3);
+
+const generateActors = () => getRandomArray(ACTORS, 3);
+
+const generateDate = () => {
+  const date = '1965-06-27';
+  dayjs(date);
+  const maxYearsGap = 10;
+  const maxMouthGap = 6;
+  const maxDayGap = 8;
+  const yearsGap = getRandomInteger(-maxYearsGap, maxYearsGap);
+  const mouthsGap = getRandomInteger(-maxMouthGap, maxMouthGap);
+  const daysGap = getRandomInteger(-maxDayGap, maxDayGap);
+  return dayjs(date).add(yearsGap, 'year').add(mouthsGap, 'month').add(daysGap, 'day').toDate();
+};
+
+const generateCountry = () => getRandomArrayElement(COUNTRES);
+
+const generateAgeRating = () => getRandomArrayElement(AGE_RATINGS);
 
 export const generateFilmCard = () => (
   {
@@ -86,10 +152,17 @@ export const generateFilmCard = () => (
     rating: generateRating(),
     year:generateYear(),
     duration: generateDuration(),
-    genre: generateGanre(),
+    genres: generateGanre(),
     poster: generatePoster(),
     description:generateDesription(),
     comments: generateComments(),
+    originalTitle: generateOriginalTitle(),
+    director: generateDirector(),
+    writers: generateWriters(),
+    actors: generateActors(),
+    releaseDate: generateDate(),
+    country: generateCountry(),
+    ageRating: generateAgeRating(),
     isAddtoWatchList: Boolean(getRandomInteger(0, 1)),
     isWhatched: Boolean(getRandomInteger(0, 1)),
     isFavorite: Boolean(getRandomInteger(0, 1)),
