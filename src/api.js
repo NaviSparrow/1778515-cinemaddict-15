c88813848ad24fc9356b4b3bd9441c408b8be04e
Ivan <work.gorobec@gmail.com>
2021-09-09 01:30:57 +0500

    добавит класс для работы с API

---
+++ src/api.js
@@ -0,0 +1,58 @@
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+};
+
+export default class Api {
+  constructor(endPoint, authorization) {
+    this._endPoint = endPoint;
+    this._authorization = authorization;
+  }
+
+  getFilms() {
+    return this._load({url: 'movies'})
+      .then(Api.toJSON);
+  }
+
+  updateFilm(film) {
+    return this._load({
+      url: `/movies/:${film.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(film),
+      headers: new Headers({'Counter-Type': 'application/json'}),
+    })
+      .then(Api.toJSON);
+  }
+
+  _load({
+    url,
+    method = Method.GET,
+    body = null,
+    headers = new Headers(),
+  }) {
+    headers.append('Authorization', this._authorization);
+
+    return fetch(
+      `${this._endPoint}/${url}`,
+      {method, body, headers},
+    )
+      .then(Api.checkStatus)
+      .then(Api.catchError);
+  }
+
+  static checkStatus(response) {
+    if (!response.ok) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+    return response;
+  }
+
+  static toJSON(response) {
+    return response.json();
+  }
+
+  static catchError(err) {
+    throw err;
+  }
+
+}
